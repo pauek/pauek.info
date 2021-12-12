062c4ce555a4cab2bbdec50aacf503fc83a1aed9
Pau Fernández <pau.fernandez@upc.edu>
2021-12-12 22:07:14 +0100

    Arreglo Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import { Layout } from '../components/Layout';
+import Script from 'next/script';
 import '../styles/globals.css'
 import '../public/webfonts/ss-social.css';
 
 function MyApp({ Component, pageProps }) {
+  const id = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
   return (
     <Layout>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+        strategy='afterInteractive'
+      />
+      <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${id}');
+      `}</Script>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,6 @@
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-
-const googleAnalyticsScript = `
-var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36758965-1']);
-    _gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-`;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -35,7 +22,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script type="text/javascript">{googleAnalyticsScript}</script>
         </body>
       </Html>
     );
