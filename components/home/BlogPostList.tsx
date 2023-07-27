fe8399ca46cd22bf0345ddbea008df7582882a1f
Pau Fernández <pau.fernandez@upc.edu>
2023-07-27 22:32:49 +0200

    Bug fix

--- components/home/BlogPostList.tsx
+++ components/home/BlogPostList.tsx
@@ -17,12 +17,12 @@ export const BlogPostList = ({ posts }: Props) => {
 const PostLink = ({ post }: { post: Post }) => {
   const [month, year] = post.date.short.split(" ");
   return (
-    <div className="flex flex-row items-baseline p-1">
-      <div className="text-stone-500 text-sm w-20 flex flex-row justify-end mr-3 ">
-        <span className="pr-1">{month}</span>
+    <div className="flex flex-row items-baseline pb-3">
+      <div className="text-stone-500 text-sm w-[4.2rem] flex flex-row justify-end mr-3">
+        <span className="pr-1">{month[0].toUpperCase()}{month.slice(1)}</span>
         <span>{year}</span>
       </div>
-      <a className="" href={post.url}>
+      <a className="flex-1" href={post.url}>
         {post.title}
       </a>
     </div>
